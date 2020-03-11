fd8fae52eba99c3570a232a573c717a337f0e859
Martina Bilyanska <2392626B@student.gla.ac.uk>
2020-03-11 23:40:15 +0000

    Created models for Courses, Reviews, Ratings, UserProfile. Star ratings plugin installed. Functionality to be tested.

--- csapp/models.py
+++ csapp/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
+from star_ratings.models import Rating
 
-# Create your models here.
+class Course(models.Model):
+#We have specified 30 for length of the name, but I will leave it 128 for now - can be changed.
+    name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=200)
+    #The list of available options organised into two groups. Contains tuples (value, human-readable name).
+    YEAR_IN_UNI_CHOICES = (
+    ('Undergraduate', (
+            ('UNDERGRAD_1YEAR', 'Undergraduate (year 1)'),
+            ('UNDERGRAD_2YEAR', 'Undergraduate (year 2)'),
+            ('UNDERGRAD_3YEAR', 'Undergraduate (year 3)'),
+            ('UNDERGRAD_1YEAR', 'Undergraduate (year 4)'),
+        )
+    ),
+    ('Postgraduate', (
+            ('POSTGRAD', 'Postgraduate'),
+        )
+    ),
+    )
+    year_in_uni = models.CharField(
+        max_length=32,
+        choices=YEAR_IN_UNI_CHOICES,
+    )
+    def __str__(self):
+        return self.name
+
+#installed: pip install django-star-ratings. Added it in installed apps in settings.py and in the urls.py. 
+# However, needs to be checked exactly how to be used. Full documentation at https://django-star-ratings.readthedocs.io/en/latest/?badge=latest/#
+# If needed, can be uninstalled and removed completely.
+class CourseRating(models.Model):
+    overall_rating = Rating
+    lecturer_rating = Rating
+    engagement = Rating
+    informative = Rating
+    def __str__(self):
+        return self.overall_rating
+
+#A class for the reviews(comments) of the course (related to the course rating many-to-one)
+class courseReview(models.Model):
+    course_rating = models.ForeignKey(CourseRating, on_delete=models.CASCADE)
+    comment = models.CharField(max_length = 250, blank = True)
+    def __str__(self):
+        return self.comment  
+        
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The additional attributes we wish to include.
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    current_student = models.BooleanField()
+    def __str__(self):
+        return self.user.username
+        
--- rate_my_cs_course/settings.py
+++ rate_my_cs_course/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'csapp',
+    'star_ratings', #star ratings functionality
 ]
 
 MIDDLEWARE = [
