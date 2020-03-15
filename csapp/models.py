3971251ac5145057035a5722273230039c1cc18e
Martina Bilyanska <2392626B@student.gla.ac.uk>
2020-03-15 20:10:23 +0000

    Change to Course model. Updated population script (all courses).

--- csapp/models.py
+++ csapp/models.py
@@ -21,10 +21,7 @@ class Course(models.Model):
 #We have specified 30 for length of the name, but I will leave it 128 for now - can be changed.
     name = models.CharField(max_length=128, unique=True)
     description = models.CharField(max_length=1000)
-    year_in_university = models.CharField(
-        max_length=32,
-        choices=YEAR_IN_UNI_CHOICES,
-    )
+    year_in_university = models.IntegerField(default=0)
     
     slug = models.SlugField(unique=True)
     
