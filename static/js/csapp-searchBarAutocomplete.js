2e058157eadea66f4bcd8bfacd2bf01061e3070c
George Iniatis <2329642I@student.gla.ac.uk>
2020-03-19 01:10:36 +0000

    Search should be working now as intended

--- static/js/csapp-searchBarAutocomplete.js
+++ static/js/csapp-searchBarAutocomplete.js
@@ -2,8 +2,25 @@ $(document).ready(function(){
 	$.ajaxSetup({ cache: false});
 	
 	$("#searchBar" ).autocomplete({
-		source: "/ratemycscourse/search/",
-		
+		source: function(request, response) {
+		$.ajax({
+			url: "/ratemycscourse/search/",
+			type: "GET",
+			dataType: "json",
+			data: {
+				term: $("#searchBar").val()
+			},
+			success: function(data) {
+				var array = $.map(data, function(item) {
+					return {
+						label: item.label,
+						value: item.value
+					};
+				});
+                response($.ui.autocomplete.filter(array, request.term));
+        }
+    });
+},
 		select: function( event, ui ) { 
 			window.location.pathname = "ratemycscourse/" + ui.item.value;
         }
